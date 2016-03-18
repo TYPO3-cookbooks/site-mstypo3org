c594165acb84a3f9470e2b1dc212ac9376b7138c
Steffen Gebert <steffen.gebert@typo3.org>
2016-03-19 00:18:40 +0100

    Add Peter's virtinstaller script

--- recipes/kvm.rb
+++ recipes/kvm.rb
@@ -7,3 +7,18 @@
 include_recipe "#{cookbook_name}::_common"
 
 include_recipe "t3-kvm"
+
+#################################
+# Virt-installer (create new VMs)
+#################################
+virt_path = "/usr/local/virtinstaller"
+
+git virt_path do
+  repository "https://github.com/TYPO3-infrastructure/virtinstaller"
+end
+
+%w{virt-install-debian}.each do |script|
+  link "/usr/local/bin/#{script}" do
+    to File.join(virt_path, script)
+  end
+end
